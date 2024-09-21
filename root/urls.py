bcf02382ef5be64cf11d43acf628de0316320256
Dibibi <paynetwww@gmail.com>
2024-09-21 22:24:00 +0500

    settings changed

--- root/urls.py
+++ root/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from root import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,5 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = 'Falcon Admin'
+
+urlpatterns += staticfiles_urlpatterns()
